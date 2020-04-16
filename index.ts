04c08ef70af1744330216975483e5224c95db993
xuancanhhv <comprocoding@gmail.com>
2020-04-16 15:47:03 +0700

    Lesson1

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 class Animal {
-  move(distanceInMeters: number = 0) {
-    console.log(`Animal moved ${distanceInMeters}m.`);
-  }
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
 }
 
-class Dog extends Animal {
-  bark() {
-    console.log("Woof! Woof!");
-  }
+class Rhino extends Animal {
+    constructor() { super("Rhino"); }
 }
 
-const dog = new Dog();
-dog.bark();
-dog.move(10);
-dog.bark();
+class Employee {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+let animal = new Animal("Goat");
+let rhino = new Rhino();
+let employee = new Employee("Bob");
+
+animal = rhino;
+animal = employee; // Error: 'Animal' and 'Employee' are not compatible
